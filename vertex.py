3b4c295f61a1bd4c48c2940e00a55faafe4fd7af
Jonathan Howard <47045231+jrh5221@users.noreply.github.com>
2019-07-22 20:17:33 -0400

    added "weight" argument to add_edge() method

--- vertex.py
+++ vertex.py
@@ -3,8 +3,8 @@ class Vertex:
     self.value = value
     self.edges = {}
 
-  def add_edge(self, vertex):
-    self.edges[vertex] = True
+  def add_edge(self, vertex, weight=0):
+    self.edges[vertex] = weight
 
   def get_edges(self):
     return list(self.edges.keys())
